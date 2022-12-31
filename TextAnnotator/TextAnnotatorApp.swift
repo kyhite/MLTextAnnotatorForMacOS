88957fa135fa8cefb9a54f8433d8b908dd508ac0
kyhite <burhi001@umn.edu>
2022-12-30 18:19:30 -0600

    Initial Commit

---
+++ TextAnnotator/TextAnnotatorApp.swift
@@ -0,0 +1,20 @@
+//
+//  TextAnnotatorApp.swift
+//  TextAnnotator
+//
+//  Created by Kyana Burhite on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TextAnnotatorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
